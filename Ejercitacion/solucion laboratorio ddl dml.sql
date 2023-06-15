6dc79b5d00825133ce6490593ea1630b29d8f440
cfpit <127329179+cfpit@users.noreply.github.com>
2023-06-14 22:13:44 -0300

    Add files via upload

--- Ejercitacion/solucion laboratorio ddl dml.sql
+++ Ejercitacion/solucion laboratorio ddl dml.sql
@@ -21,6 +21,55 @@ values(null,'Pedro','Car','1254-9658','pedrocar@hotmail.com','1971-02-01',10.56,
 
 select * from alumnos;
 
+-- Comando INSERT
+-- Ejercicio #1
+-- • Insertar en la tabla alumnos los siguentes alumnos: 
+-- Comando SELECT
+-- Ejercicio #1
+-- • Seleccionar todos los alumnos
+select * from alumnos;
+-- Ejercicio #2
+-- • Seleccionar el alumno que tenga como identificador el numero 3
+select * from alumnos where alumnoid = 3;
+-- • Seleccionar el alumno que tenga como identificador el numero 3 y 5
+select * from alumnos where alumnoid = 3 and alumnoid = 5;
+-- • Seleccionar el alumno que tenga como identificador el numero 3 o 5
+select * from alumnos where alumnoid = 3 or alumnoid = 5;
+-- Ejercicio #3
+-- • Utilización de la clausula LIKE y los %
+-- • Seleccionar los alumnos que contengan “erez” en su apellido
+select * from alumnos where apellido like '%erez%';
+-- • Consultar todos los alumnos que tengan cuenta de gmail 
+select * from alumnos where email like '%gmail%';
+-- Ejercicio #4
+-- • Consultar todos los alumnos cuyo numero este comprendido entre 3 y 6
+select * from alumnos where alumnoid between 3 and 6;
+-- • Consultar todos los alumnos cuyo numero este comprendido dentro de la
+-- siguiente lista: 1, 3 , 4 , 8
+select * from alumnos where alumnoid in (1, 3 , 4 , 8);
+-- Comando DELETE
+-- Ejercicio #1
+-- • Eliminar al alumno “Jose Cabrera”
+delete from alumnos where nombre = 'jose' and apellido = 'cabrera';
+-- Ejerrcicio #2
+-- • Eliminar los alumnos que tengan como apellido a “Perez”
+delete from alumnos where apellido = 'perez';
+-- Comando UPDATE
+-- Ejerrcicio #1
+-- • Actualizar el mail actual de Pedro Car por “pedrito@car.com” y su telefono por
+-- “2020-45896”
+update	alumnos 
+set 	email = 'pedrito@car.com', telefono = '2020-45896'
+where	nombre = 'pedro' and apellido = 'car';
+-- • Incrementar los saldos en un 10 %
+update	alumnos 
+set 	saldo = saldo * 1.1;
+-- • Decrementar los saldos en un 15 % de aquellos alumnos que sean menores de
+-- edad 
+update	alumnos 
+set 	saldo = saldo * 0.85
+where	edad < 18;
+
 
 
 
